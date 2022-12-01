222d931e14f25296e1e805e7b74181ef027d92eb
Nikita Zlochevskyi <nik102938@gmail.com>
2022-12-01 04:17:37 +0200

    Fixed camera

--- Assets/Scripts/View/Camera/CameraMovement.cs
+++ Assets/Scripts/View/Camera/CameraMovement.cs
@@ -6,17 +6,16 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private Vector3 _offset;
     [SerializeField] private float _smoothTime;
 
-    [SerializeField] private Rigidbody _rigidbody;
-    private Vector3 _velocity;
+    private Vector3 _velocity = Vector3.zero;
 
-    private void Update()
+    private void FixedUpdate()
     {
         MoveCamera();
     }
 
     private void MoveCamera()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, _ball.position + _offset,
-            ref _velocity, _rigidbody.velocity.y * Time.deltaTime);
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(0, 0, _ball.position.z) + _offset,
+            ref _velocity, _smoothTime);
     }
 }
